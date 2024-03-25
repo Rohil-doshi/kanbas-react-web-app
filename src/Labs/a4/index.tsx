eb79a7423d70fb873d0bd5f37729fe6126a73005
Rohil-doshi <rohil@Rohils-MacBook-Pro.local>
2024-03-25 01:53:06 -0400

    completed until store

--- src/Labs/a4/index.tsx
+++ src/Labs/a4/index.tsx
@@ -1,9 +1,63 @@
 import React from "react";
+import ClickEvent from "./ClickEvent";
+import PassingDataOnEvent from "./PassingDataOnEvent";
+import PassingFunctions from "./PassingFunctions";
+import EventObject from "./EventObject";
+import Counter from "./Counter";
+import BooleanStateVariables from "./BooleanStateVariables";
+import StringStateVariables from "./StringStateVariables";
+import DateStateVariable from "./DateStateVariable";
+import ObjectStateVariable from "./ObjectStateVariable";
+import ArrayStateVariable from "./ArrayStateVariable";
+import ParentStateComponent from "./ParentStateComponent";
+import ReduxExamples from "./ReduxExamples";
 
 const Assignment4 = () => {
+    function sayHello() {
+        alert("Hello");
+      }
+
+      
   return(
     <>
       <h1>Assignment 4</h1>
+
+      <br/>
+      <ClickEvent/>
+
+      <br/>
+      <PassingDataOnEvent/>
+
+      <br/>
+      <PassingFunctions theFunction={sayHello} />
+
+      <br/>
+      <EventObject/>
+
+      <br/>
+      <Counter/>
+
+      <br/>
+      <BooleanStateVariables/>
+
+      <br/>
+      <StringStateVariables/>
+
+      <br/>
+      <DateStateVariable/>
+
+      <br/>
+      <ObjectStateVariable/>
+
+      <br/>
+      <ArrayStateVariable/>
+
+      <br/>
+      <ParentStateComponent/>
+      
+      <br/>
+      <ReduxExamples/>
+
     </>
   );
 };
