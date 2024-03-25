984a41c736dda7edf8dc1f6eda5e4cab6d952816
Rohil-doshi <rohil@Rohils-MacBook-Pro.local>
2024-03-25 01:54:35 -0400

    just added store

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -2,10 +2,13 @@ import Assignment3 from "./a3";
 import Nav from "../Nav";
 import { Routes, Route, Link } from "react-router-dom";
 import Assignment4 from "./a4";
+import store from "./store";
+import { Provider } from "react-redux";
 
 
 function Labs() {
   return (
+    <Provider store={store}>
     <div className="container-fluid">
       <h1>Labs</h1>
       <Nav />
@@ -16,6 +19,7 @@ function Labs() {
         <Route path="/a4" element={<Assignment4 />} />
       </Routes>
     </div>
+    </Provider>
   );
 }
 export default Labs;
