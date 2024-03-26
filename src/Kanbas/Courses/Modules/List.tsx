8953058f6b1e1de318fb6ac35076f488e37e9f23
Rohil-doshi <rohil@Rohils-MacBook-Pro.local>
2024-03-25 23:29:44 -0400

    completed until 3.3

--- src/Kanbas/Courses/Modules/List.tsx
+++ src/Kanbas/Courses/Modules/List.tsx
@@ -5,18 +5,57 @@ import { FaEllipsisV, FaCheckCircle, FaPlusCircle } from "react-icons/fa";
 import { useParams } from "react-router";
 const {modules} = Database;
 
-function ModuleList() { 
+function ModuleList() {
   const { courseId } = useParams();
-  const modulesList = modules.filter((module) => module.course === courseId);
-  const [selectedModule, setSelectedModule] = useState(modulesList[0]);
+  const [moduleList, setModuleList] = useState<any[]>(modules);
+  const [selectedModule, setSelectedModule] = useState(moduleList[0]);
+
+  const [module, setModule] = useState({
+    name: "New Module",
+    description: "New Description",
+    course: courseId,
+  });
+
+  const addModule = (module: any) => {
+    const newModule = { ...module,
+      _id: new Date().getTime().toString() };
+    const newModuleList = [newModule, ...moduleList];
+    setModuleList(newModuleList);
+  };
+
+  const deleteModule = (moduleId: string) => {
+    const newModuleList = moduleList.filter(
+      (module) => module._id !== moduleId );
+    setModuleList(newModuleList);
+  };
+
+
+
   return (
     <>
       {/* <!-- Add buttons here --> */}
-      <ul className="list-group wd-modules">
-        {modulesList.map((module, index) => (
-          <li key={index}
-            className="list-group-item"
-            onClick={() => setSelectedModule(module)}>
+      <ul className="list-group">
+
+        {/* New code for buttons  */}
+
+        <li className="list-group-item">
+        <button className="btn btn-success" onClick={() => { addModule(module) }}>Add</button>
+        <input className= "m-2" value={module.name} onChange={(e) => setModule({...module, name: e.target.value })}/>
+        <textarea className="m-2" value={module.description} onChange={(e) => setModule({...module, description: e.target.value })}/>
+      </li>
+
+
+
+        {moduleList
+        .filter((module) => module.course === courseId)
+        .map((module, index) => (
+          <li key={index} className="list-group-item m-1" onClick={() => setSelectedModule(module)}>
+            {/* Button for deleting modules */}
+              <button className="btn btn-danger m-2"
+              onClick={() => deleteModule(module._id)}>
+              Delete
+            </button>
+
             <div>
               <FaEllipsisV className="me-2" />
               {module.name}
@@ -26,20 +65,23 @@ function ModuleList() {
                 <FaEllipsisV className="ms-2" />
               </span>
             </div>
-            {selectedModule._id === module._id && (
-              <ul className="list-group">
-                {module.lessons?.map((lesson, index) => (
-                  <li className="list-group-item" key={index}>
-                    <FaEllipsisV className="me-2" />
-                    {lesson.name}
-                    <span className="float-end">
-                      <FaCheckCircle className="text-success" />
-                      <FaEllipsisV className="ms-2" />
-                    </span>
-                  </li>
-                ))}
-              </ul>
-            )}
+            {selectedModule._id === module._id
+            // Ask professor for help with this part!! 
+            // (
+            //   <ul className="list-group">
+            //     {module.lessons?.map((lesson, index) => (
+            //       <li className="list-group-item" key={index}>
+            //         <FaEllipsisV className="me-2" />
+            //         {lesson.name}
+            //         <span className="float-end">
+            //           <FaCheckCircle className="text-success" />
+            //           <FaEllipsisV className="ms-2" />
+            //         </span>
+            //       </li>
+            //     ))}
+            //   </ul>
+            // )
+            }
           </li>
         ))}
       </ul>
